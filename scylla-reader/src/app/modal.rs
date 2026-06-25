d0ffbd511b12a999c47f9a3de9900c726643a4ee
MothInBOX <judegilchrist2006@gmail.com>
2026-06-25 14:23:43 +0100

    Missed some files in last commit

---
+++ scylla-reader/src/app/modal.rs
@@ -0,0 +1,15 @@
+use crate::models::Chapter;
+
+pub enum Modal {
+    None,
+    AddBook {
+        inputs: Vec<String>,
+        cursor: usize,
+        scroll_offset: usize,
+    },
+    JumpChapter {
+        chapters: Vec<Chapter>,
+        cursor: usize,
+        scroll_offset: usize,
+    },
+}
