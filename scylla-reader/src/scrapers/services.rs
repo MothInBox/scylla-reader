3deb9f48738bb8dc33ce6e2aead97dbdd8126fa4
Jude Gilchrist <106194049+MothInBox@users.noreply.github.com>
2026-07-05 18:11:47 +0100

    Fix JSON serialization in services.rs

--- scylla-reader/src/scrapers/services.rs
+++ scylla-reader/src/scrapers/services.rs
@@ -94,6 +94,7 @@ impl ScraperRegistry {
             obj.remove("_cookies");
             obj.remove("_accepts_cookies");
         }
+    Some(serde_json::to_string(&json).ok()?)
     }
 
     pub async fn scrape_url(
