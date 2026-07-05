8336ff45ca4b5a3ca24e8be9f66853cb2af56030
MothInBOX <judegilchrist2006@gmail.com>
2026-07-05 21:18:50 +0100

    chore: bump version to 0.2.0

--- scylla-reader/package.nix
+++ scylla-reader/package.nix
@@ -7,7 +7,7 @@
 }:
 rustPlatform.buildRustPackage {
   pname = "scylla-reader";
-  version = "0.1.0";
+  version = "0.2.0";
   src = ../.;
   cargoRoot = "scylla-reader";
   buildAndTestSubdir = "scylla-reader";
